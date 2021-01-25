5d8bb6f37b4ceac83d7c199aa1815db471d0a37e
Gustavo Lemos <gglemos21@gmail.com>
2021-01-25 10:12:52 -0300

    Not initiate game immediately

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc snake.c game.c spawn.c collisions.c draw.c ends.c update.c input.c -lncurses -o snake && ./snake
+gcc snake.c game.c spawn.c collisions.c draw.c ends.c update.c input.c -lncurses -o snake
